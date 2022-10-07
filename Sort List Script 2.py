45cf2afa41f41d6d6f983ab3930652f7ad95b560
Austin Wright <auwright@alumni.nmu.edu>
2022-10-07 00:52:32 -0700

    script add

---
+++ Sort List Script 2.py
@@ -0,0 +1,50 @@
+mylistFL = ["Rainbow River",
+          "Null"]
+
+mylistMI = ["Au Sable River",
+          "Burt Lake",
+          "Douglas Lake",
+          "Grand Traverse Bay",
+          "Lake Michigan",
+          "Lake Superior",
+          "Larks Lake",
+          "Twin Lakes",
+          "Two Hearted River",]
+
+mylistMT = ["Bitterroot River",
+          "Lake Levinsky",
+          "Lake McDonald",]
+
+mylistOR = ["Clear Lake",
+          "Elk Lake",
+          "Fall Creek Lake",
+          "Hosmer Lake",
+          "Lookout Point Lake",
+          "Lost Lake",
+          "Scott Lake",
+          "Timothy Lake",
+          "Waldo Lake",
+          "Wickiup Reservior"]
+
+mylistWA = ["Wenas Lake",
+          "Null"]
+
+mylistFL.sort()
+print('FL')
+print(mylistFL)
+
+mylistMI.sort()
+print('MI')
+print(mylistMI)
+
+mylistMT.sort()
+print('MT')
+print(mylistMT)
+
+mylistOR.sort()
+print('OR')
+print(mylistOR)
+
+mylistWA.sort()
+print('WA')
+print(mylistWA)
