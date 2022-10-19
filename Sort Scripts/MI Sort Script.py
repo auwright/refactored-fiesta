c3e0df22f1b4a71bb921cf95b904a2f90026e486
Austin Wright <auwright@alumni.nmu.edu>
2022-10-18 22:15:27 -0700

    added mi script

---
+++ Sort Scripts/MI Sort Script.py
@@ -0,0 +1,23 @@
+mylistMI = ["Au Sable River",
+          "Burt Lake",
+          "Douglas Lake",
+          "Grand Traverse Bay",
+          "Lake Michigan",
+          "Lake Superior",
+          "Larks Lake",
+          "Twin Lakes",
+          "Two Hearted River",]
+
+mylistMI.sort()
+
+idMI = ["1MI) ", "2MI) ", "3MI) ", "4MI) ", "5MI) ", "6MI) ", "7MI) ", "8MI) ", "9MI) "]
+
+print(idMI[0] + mylistMI[0])
+print(idMI[1] + mylistMI[1])
+print(idMI[2] + mylistMI[2])
+print(idMI[3] + mylistMI[3])
+print(idMI[4] + mylistMI[4])
+print(idMI[5] + mylistMI[5])
+print(idMI[6] + mylistMI[6])
+print(idMI[7] + mylistMI[7])
+print(idMI[8] + mylistMI[8])
