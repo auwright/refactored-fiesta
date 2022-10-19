6c0cdb17628d180fc60018c4527f3f83732106c3
Austin Wright <auwright@alumni.nmu.edu>
2022-10-18 22:29:29 -0700

    revised fl script

--- Sort Scripts/FL Sort Script.py
+++ Sort Scripts/FL Sort Script.py
@@ -1,25 +1,9 @@
-mylistOR = ["Clear Lake",
-          "Elk Lake",
-          "Fall Creek Lake",
-          "Hosmer Lake",
-          "Lookout Point Lake",
-          "Lost Lake",
-          "Scott Lake",
-          "Waldo Lake",
-          "Wickiup Reservior",
-          "Timothy Lake"]
+mylistFL = ["Rainbow River",
+          "Null"]
 
-mylistOR.sort()
+mylistFL.sort()
 
-idOR = ["1OR) ", "2OR) ", "3OR) ", "4OR) ", "5OR) ", "6OR) ", "7OR) ", "8OR) ", "9OR) ", "10OR) "]
+idFL = ["1FL) ", "2FL) "]
 
-print(idOR[0] + mylistOR[0])
-print(idOR[1] + mylistOR[1])
-print(idOR[2] + mylistOR[2])
-print(idOR[3] + mylistOR[3])
-print(idOR[4] + mylistOR[4])
-print(idOR[5] + mylistOR[5])
-print(idOR[6] + mylistOR[6])
-print(idOR[7] + mylistOR[7])
-print(idOR[8] + mylistOR[8])
-print(idOR[9] + mylistOR[9])
+print(idFL[0] + mylistFL[0])
+print(idFL[1] + mylistFL[1])
